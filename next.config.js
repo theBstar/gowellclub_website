b9d2a354ae3276ab326d04d0bec873ad10e35bd3
bikram <6bikramsutar@gmail.com>
2024-06-28 01:14:20 +0530

    remove basepath

--- next.config.js
+++ next.config.js
@@ -1,24 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-let basePath = "";
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-  basePath = `/${repo}`;
-}
-
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
-  basePath: basePath,
-  env: {
-    NEXT_PUBLIC_BASE_PATH: process.env.NEXT_PUBLIC_BASE_PATH || "",
-  },
-  images: {
-    loader: "imgix",
-    path: `${process.env.NEXT_PUBLIC_BASE_PATH}/`,
-  },
 };
 
 module.exports = nextConfig;
